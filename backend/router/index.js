3730d98c2230e0f0aca90fdb7f6b58f65fbf7ee0
MegaLoogin <veh23sab@yandex.ru>
2024-10-15 20:12:56 +0300

    Update index.js

--- backend/router/index.js
+++ backend/router/index.js
@@ -24,6 +24,7 @@ router.post('/sendContact', async (req, res, next) => {
         const resp = await fetch(`https://api.telegram.org/bot${process.env.TG_TOKEN}/sendMessage?chat_id=${process.env.CHAT_ID}&parse_mode=html&text=${txt}`);
         return res.status(resp.status);
     }catch(e){
+        console.log(e);
         next(e);
     }
 });
